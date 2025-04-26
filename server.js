e4bc2093fc349c20c27e20ea4764836f15920bbf
Cristian Alejandro Arroyave <cristianalejo84@gmail.com>
2025-04-26 19:29:37 +0200

    cambio de ip

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const http = require('http');
 
 // Obtener el puerto del entorno o usar el 3000 por defecto
 const port = process.env.PORT || 3002;
+const host = process.env.HOST || '0.0.0.0';
 app.set('port', port);
 
 // Crear el servidor HTTP
 const server = http.createServer(app);
 
 // Escuchar en el puerto especificado
-server.listen(port);
+server.listen(port, host);  
 
 // Manejar eventos del servidor
 server.on('error', (error) => {
